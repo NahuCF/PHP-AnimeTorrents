2a8411bcbf9c25c4c6841bda4d4df615dce4151d
Nahuel <nahuefer105@outlook.com>
2021-03-02 04:49:57 -0300

    Idk i never know...

--- view/index.view.php
+++ view/index.view.php
@@ -13,30 +13,32 @@
 <body>
     <header>
     </header>
-    <table>
-        <thead>
-            <tr>
-                <th class="thead__name">Name</th>
-                <th class="thead__link">Link</th>
-                <th class="thead__size"><a href="#">Size</a></th>
-                <th class="thead__date">Date</th>
-                <th class="thead__uparrow"><a href="#"><i class="fas fa-arrow-down"></i></a></th>
-                <th class="thead__downarrow"><a href="#"><i class="fas fa-arrow-up"></i></a></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>ins</td>
-                <td class="text-center">
-                    <a href="#"><i class="fas fa-download"></i></a>
-                    <a href="#"><i class="fas fa-magnet"></i></a>
-                </td>
-                <td class="text-center">asd</td>
-                <td class="text-center">asd</td>
-                <td class="text-center">1</td>
-                <td class="text-center">2</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="wraper">
+        <table>
+            <thead>
+                <tr>
+                    <th class="thead__name">Name</th>
+                    <th class="thead__link">Link</th>
+                    <th class="thead__size"><a href="#">Size</a></th>
+                    <th class="thead__date">Date</th>
+                    <th class="thead__uparrow"><a href="#"><i class="fas fa-arrow-down"></i></a></th>
+                    <th class="thead__downarrow"><a href="#"><i class="fas fa-arrow-up"></i></a></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="colaps"><a href="#">Banished from the Hero's Party, I Decided to Live a Quiet Life in the Countryside v01-02 [Yen Press] [LuCaZ]</a></td>
+                    <td class="text-center">
+                        <a href="#"><i class="fas fa-download"></i></a>
+                        <a href="#"><i class="fas fa-magnet"></i></a>
+                    </td>
+                    <td class="text-center">153.3 MB</td>
+                    <td class="text-center">2021-03-02 02:44</td>
+                    <td class="text-center">1</td>
+                    <td class="text-center">2</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
