0dfed8b66891384e91944d71be6bdd1a0c5f95e9
Nahuel <nahuefer105@outlook.com>
2021-03-06 23:12:28 -0300

    Pagination without browse or filters

--- view/pagination.php
+++ view/pagination.php
@@ -1,8 +1,26 @@
+<?php $total_pages = number_of_pages($page_config["torrents_per_page"], $conection); ?>
 <ul class="pagination">
-    <li class="disabled"><a href="#">&laquo;</a></li>
-    <li class="active"><a href="1">1</a></li>
-    <li class="active"><a href="2">2</a></li>
-    <li class="active"><a href="3">3</a></li>
-    <li class="active"><a href="1">4</a></li>
-    <li class="pagination_right"><a href="">&raquo;</a></li>
+    
+    <?php if(get_page() == 1): ?>
+        <li class="left__aquo-disabled">&laquo;</li>
+    <?php else: ?>
+        <li><a id="left__aquo-enabled" href="<?php echo "?p=" . get_page() - 1; ?>">&laquo;</a></li>
+    <?php endif; ?>
+
+    <?php for($i = 1; $i <= $total_pages; $i++): ?>
+        <?php if(get_page() == $i): ?>
+            <li class="active">
+                <a href="<?php echo "?p=" . $i ?>"><?php echo $i; ?></a>
+            </li>   
+        <?php else: ?>
+            <li><a href="<?php echo "?p=" . $i ?>"><?php echo $i; ?></a></li>
+        <?php endif; ?>
+    <?php endfor; ?>
+    
+    <?php if(get_page() == $total_pages): ?>
+        <li class="right__aquo-disabled">&raquo;</li>
+    <?php else: ?>
+        <li><a id="right__aquo-enabled" href="<?php echo "?p=" . get_page() + 1; ?>">&raquo;</li>
+    <?php endif; ?>
+
 </ul>
