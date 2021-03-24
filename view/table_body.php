a9af0da4e33826034be4d454a22d77fe7ce57796
Nahuel <nahuefer105@outlook.com>
2021-03-24 03:40:04 -0300

    Code of table_body.php simplified with nth_child() in the CSS

--- view/table_body.php
+++ view/table_body.php
@@ -1,39 +1,17 @@
-<?php $even_number = 0; ?>
 <?php foreach($torrents as $torrent): ?>
-    <?php if($even_number % 2 == 0): ?>
-        <tr class="row__table row__table-grey">
-            <td class="colaps"><a href="<?php echo "single.php" . "?ID=" . $torrent["ID"]; ?>"><?php echo $torrent["name"]; ?></a></td>
-            <td class="text-center">
-                <a href="<?php echo "dowload.php?f=" . $torrent["ID"]; ?>"><i class="fas fa-download"></i></a>
-                <?php if(!empty($torrent["magnet"])): ?>
-                    <a href="<?php echo $torrent["magnet"] ?>"><i class="fas fa-magnet"></i></a>
-                <?php else: ?>
-                    <a href="#" id="magnet__noclick" ?><i class="fas fa-magnet" id="magnet__red"></i></a>
-                <?php endif; ?>
-            </td>
-            <td class="text-center hide"><?php echo bytes_to_string($torrent["size"]); ?></td>
-            <td class="text-center hide"><?php echo date('Y-m-d H:i', strtotime($torrent["date"])); ?></td>
-            <td class="text-center" style="color: green;"><?php echo $torrent["likes"]; ?></td>
-            <td class="text-center" style="color: red;"><?php echo $torrent["dislikes"]; ?></td>
-        </tr>
-    <?php else: ?>
-        <tr class="row__table row__table-white">
-            <td class="colaps"><a href="<?php echo "single.php" . "?ID=" . $torrent["ID"]; ?>"><?php echo $torrent["name"]; ?></a></td>
-            <td class="text-center">
-                <a href="<?php echo "dowload.php?f=" . $torrent["ID"]; ?>"><i class="fas fa-download"></i></a>
-                <?php if(!empty($torrent["magnet"])): ?>
-                    <a href="<?php echo $torrent["magnet"] ?>"><i class="fas fa-magnet"></i></a>
-                <?php else: ?>
-                    <a href="#" id="magnet__noclick"><i class="fas fa-magnet" id="magnet__red"></i></a>
-                <?php endif; ?>
-            </td>
-            <td class="text-center hide"><?php echo bytes_to_string($torrent["size"]); ?></td>
-            <td class="text-center hide"><?php echo date('Y-m-d H:i', strtotime($torrent["date"])); ?></td>
-            <td class="text-center" style="color: green;"><?php echo $torrent["likes"]; ?></td>
-            <td class="text-center" style="color: red;"><?php echo $torrent["dislikes"]; ?></td>
-        </tr>
-    <?php endif; ?>
-
-    <?php $even_number++; ?>
-
+    <tr class="row__table">
+        <td class="colaps"><a href="<?php echo "single.php" . "?ID=" . $torrent["ID"]; ?>"><?php echo $torrent["name"]; ?></a></td>
+        <td class="text-center">
+            <a href="<?php echo "dowload.php?f=" . $torrent["ID"]; ?>"><i class="fas fa-download"></i></a>
+            <?php if(!empty($torrent["magnet"])): ?>
+                <a href="<?php echo $torrent["magnet"] ?>"><i class="fas fa-magnet"></i></a>
+            <?php else: ?>
+                <a href="#" style="pointer-events: none;" ?><i class="fas fa-magnet" id="magnet__red"></i></a>
+            <?php endif; ?>
+        </td>
+        <td class="text-center hide"><?php echo bytes_to_string($torrent["size"]); ?></td>
+        <td class="text-center hide"><?php echo date('Y-m-d H:i', strtotime($torrent["date"])); ?></td>
+        <td class="text-center" style="color: green;"><?php echo $torrent["likes"]; ?></td>
+        <td class="text-center" style="color: red;"><?php echo $torrent["dislikes"]; ?></td>
+    </tr>
 <?php endforeach; ?>
