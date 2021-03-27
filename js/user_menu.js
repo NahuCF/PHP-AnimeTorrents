178d9d824767eae78e09e9b33de78f04aa00f992
Nahuel <nahuefer105@outlook.com>
2021-03-27 17:58:36 -0300

    Responsive header

--- js/user_menu.js
+++ js/user_menu.js
@@ -1,26 +1,22 @@
-var active = false;
 var btn = document.getElementById("user__type");
-var subMenu = document.getElementById("switch");
-var userType = document.getElementById("user__type");
+var subMenu = document.getElementById("sub__menu");
 
 btn.addEventListener("click", function()
 {
-    if(active == true)
+    if(subMenu.style.display != "none")
     {
-        active = false;
+        subMenu.style.display = "none";
+        subMenu.pointerEvents = "none";
 
-        subMenu.style.opacity = 0;
-        subMenu.style.pointerEvents = "none";
-        userType.style.color = "#9D9D9D";
-        userType.style.backgroundColor = "#222222";
+        btn.style.backgroundColor = "#222222";
+        btn.style.color = "#9D9D9D";
     }
     else
     {
-        active = true;
-
-        subMenu.style.opacity = 1;
+        subMenu.style.display = "block";
         subMenu.style.pointerEvents = "auto";
-        userType.style.color = "white";
-        userType.style.backgroundColor = "#080808";
+
+        btn.style.backgroundColor = "#080808";
+        btn.style.color = "white";
     }
 });
--- view/header.php
+++ view/header.php
@@ -1,57 +1,73 @@
 <header>
-    <a href="index.php">Inicio</a>
-    <?php if(isset($_SESSION["user"])): ?>
-        <a href="upload.php">UPLOAD</a>
-    <?php endif; ?>
-    <form action="search.php" method="GET">
-        <?php if(isset($_GET["u"])): ?>
-            <input name="u" value="<?php echo clean_string($_GET['u']); ?>" style="pointer-events: none; display: none" type="text">
+    <div class="header-top">
+        <a style="font-size: 18px;" href="index.php">Nyaa Copy</a>
+        <button id="hamburger-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    </div>
+
+    <div class="header-bottom-container" id="header-bottom-container">
+        <?php if(isset($_SESSION["user"])): ?>
+            <div><a href="upload.php">Upload</a></div>
         <?php endif; ?>
-        <input class="input_search" name="w" type="text" placeholder="<?php echo isset($_GET["u"]) ? "Search " . clean_string($_GET["u"]) . "'" . " torrents" : "Search..." ?>">
-        <div class="button__headercontenedor" >
-            <button class="header__submitbtn" type="submit"><i class="fa fa-search fa-fw"></i></button>
+
+        <div class="header-bottom">
+            <form class="form-header" action="search.php" method="GET">
+                <?php if(isset($_GET["u"])): ?>
+                    <input name="u" value="<?php echo clean_string($_GET['u']); ?>" style="pointer-events: none; display: none" type="text">
+                <?php endif; ?>
+                <input class="input_search" name="w" type="text" placeholder="<?php echo isset($_GET["u"]) ? "Search " . clean_string($_GET["u"]) . "'" . " torrents" : "Search..." ?>">
+                <div class="button__headercontenedor" >
+                    <button class="header__submitbtn" type="submit"><i class="fa fa-search fa-fw"></i></button>
+                </div>
+            </form>
+
+            <ul class="user__list">
+                <li class="first__list">
+                    <a class="user__type" id="user__type" href="#">
+                        <i class="far fa-user"></i>
+                        <?php if(isset($_SESSION["user"])): ?>
+                            <?php echo $_SESSION["user"]; ?>
+                        <?php else: ?>
+                            <?php echo "Guest" ?>
+                        <?php endif; ?>
+                        <i class="fas fa-caret-down"></i>
+                    </a>
+                    <ul class="sub__menu" id="sub__menu" style="display: none;">
+                        <?php if(isset($_SESSION["user"])): ?>
+                            <li>
+                                <a href="profile.php">
+                                    <i class="fas fa-cog"></i>
+                                    Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="logout.php">
+                                    <i class="fa fa-times fa-fw"></i>
+                                    Logout
+                                </a>
+                            </li>
+                        <?php else: ?>
+                            <li>
+                                <a href="login.php">
+                                    <i class="fas fa-sign-in-alt"></i>
+                                    Login
+                                </a>
+                            </li>
+                            <li>
+                                <a href="register.php">
+                                    <i class="fas fa-pencil-alt"></i>
+                                    Register
+                                </a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </li>
+            </ul>   
         </div>
-    </form>
-    <ul class="user__list">
-        <li class="first__list">
-            <a class="user__type" id="user__type" href="#">
-                <i class="far fa-user"></i>
-                <?php if(isset($_SESSION["user"])): ?>
-                    <?php echo $_SESSION["user"]; ?>
-                <?php else: ?>
-                    <?php echo "Guest" ?>
-                <?php endif; ?>
-                <i class="fas fa-caret-down"></i>
-            </a>
-            <ul class="sub__menu" id="switch">
-                <?php if(isset($_SESSION["user"])): ?>
-                    <li>
-                        <a href="profile.php">
-                            <i class="fas fa-cog"></i>
-                            Profile
-                        </a>
-                    </li>
-                    <li>
-                        <a href="logout.php">
-                            <i class="fa fa-times fa-fw"></i>
-                            Logout
-                        </a>
-                    </li>
-                <?php else: ?>
-                    <li>
-                        <a href="login.php">
-                            <i class="fas fa-sign-in-alt"></i>
-                            Login
-                        </a>
-                    </li>
-                    <li>
-                        <a href="register.php">
-                            <i class="fas fa-pencil-alt"></i>
-                            Register
-                        </a>
-                    </li>
-                <?php endif; ?>
-            </ul>
-        </li>
-    </ul>
+    </div>
 </header>
+
+<?php echo '<script type="text/javascript" src="js/header.js"></script>'; ?>
